6e9c4fc7375f856e82e2a6f6605cc5cf4bbacf13
chisnikoff <45393985+chisnikoff@users.noreply.github.com>
2024-09-12 22:33:39 +0700

    add files

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from num2words import num2words
 print(translit("""\
 Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. 
 People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. 
-Neither will happen.
-
+Neither will happen.\n
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...\n""", 'ru'))
-
 print('78', num2words(78), sep = ' - ')
 print('15', num2words(15), sep = ' - ')
 print('3', num2words(3), sep = ' - ')
