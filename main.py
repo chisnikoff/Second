fc24f1150da31465b344ace12d466629ae84be7f
chisnikoff <45393985+chisnikoff@users.noreply.github.com>
2024-09-13 15:02:21 +0700

    Исправления и удаления лишних строк

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import transliterate
-import num2words
 from transliterate import translit
 from num2words import num2words
 print(translit("""\
@@ -7,9 +5,9 @@ Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once
 People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. 
 Neither will happen.\n
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...\n""", 'ru'))
-print('78', num2words(78), sep = ' - ')
-print('15', num2words(15), sep = ' - ')
-print('3', num2words(3), sep = ' - ')
-print('40', num2words(40), sep = ' - ')
-print('8', num2words(8), sep = ' - ')
+print('78', translit(num2words(78), 'ru'), sep = ' - ')
+print('15', translit(num2words(15), 'ru'), sep = ' - ')
+print('3', translit(num2words(3), 'ru'), sep = ' - ')
+print('40', translit(num2words(40), 'ru'), sep = ' - ')
+print('8', translit(num2words(8), 'ru'), sep = ' - ')
 
